23fc88e537f02fa106091b968961fb87f9d5ee31
jaimiepaulson <jaimiepaulson@yahoo.com>
2025-05-17 14:53:11 -0400

    Initial commit

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+function showNavigation() {
+  var navigation = document.getElementById("navigation");
+  if (navigation.style.display === "block") {
+    navigation.style.display = "none";
+  } else {
+    navigation.style.display = "block";
+  }
+}
